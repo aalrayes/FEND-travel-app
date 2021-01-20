8d6d7dc379d8fa78902af923a9d84157346057e0
aalrayes <a.alrayes.404@gmail.com>
2021-01-20 03:03:54 +0300

    new improvement

--- src/client/index.js
+++ src/client/index.js
@@ -4,12 +4,15 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/fonts.scss'
+import './styles/trip.scss'
 import {init} from "./js/app"
+import {startServiceWorker} from "./js/serviceWorker"
+
 const submit =  document.getElementById('submit');
-
-
 submit.addEventListener('click', e=> {
     init(e);
 });
-
-export{init}
+export{
+    init,
+    startServiceWorker
+}
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,12 +1,12 @@
+import { container } from "webpack";
+
 let trips = {};
-let d = new Date();
-let newDate = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear();
+const months = ['01','02','03','04',"05","06","07",'08','09','10','11','12'];
 
 // Weather Bit API
-const weatherbit_API_url ="https://api.weatherbit.io/v2.0/forecast/daily?";
-const weatherbit_API_type = ['current','forecast'];
+const weatherbit_API_url ="https://api.weatherbit.io/v2.0/";
+const weatherbit_API_type = ['normals?','forecast/daily?'];
 const weatherbit_API_key='&key=ea7555658fe34b60a2c63e087a520673';
-const days = '&days=7';
 // Geonames API
 const geonames_API_url ="http://api.geonames.org/searchJSON?name_equals=";
 const geonames_API_key="&username=aalrayes100x";
@@ -20,7 +20,9 @@ const pixabay_API_key = "?key=19924293-20d70f0423878e94f2469f599";
 function init(e) {
   e.preventDefault();
   const city = document.getElementById('input').value;
-  const days = document.getElementById('date').value;
+  const trip_date = document.getElementById('datepicker').value;
+  const daysToDate = calculateDaysToDate(new Date(trip_date));
+
   console.log(city)
   getGeoname(geonames_API_url,city,geonames_API_key)
     .then(function (data) 
@@ -31,6 +33,23 @@ function init(e) {
         country: data.geonames[0].countryName
       });
 
+      getWeatherBit(weatherbit_API_url,data.geonames[0].lng,data.geonames[0].lat,trip_date,weatherbit_API_key)
+      .then(function(weatherData){
+        let descriptionFromAPI;
+        if(daysToDate > 7){
+            descriptionFromAPI = "";
+        }else{
+          descriptionFromAPI = weatherData.data[0].weather.description;
+        }
+
+        postData('http://localhost:3000/weatherbit', { 
+          low_temp:weatherData.data[0].low_temp,
+          max_temp: weatherData.data[0].max_temp,
+          description: descriptionFromAPI
+        });
+        generateContent(city,trip_date)
+      })
+
       getPixabay(pixabay_API_url,city,pixabay_API_key)
       .then(function(data)
       {
@@ -39,31 +58,20 @@ function init(e) {
         });
 
       });
-      getWeatherBit(weatherbit_API_url,data.geonames[0].lng,data.geonames[0].lat,days,weatherbit_API_key)
-      .then(function(weatherData){
-        
-        let arrLow =[];
-        let arrMax =[];
-        let arrDes =[];
-
-        weatherData.data.forEach(day => {
-          arrLow.push(day.low_temp);
-          arrMax.push(day.max_temp);
-          arrDes.push(day.weather.description);
-        });
-
-        postData('http://localhost:3000/weatherbit', { 
-          low_temp: arrLow,
-          max_temp: arrMax,
-          description: arrDes
-        });
-        generateContent()
-      })
 
     })
 }
 const getWeatherBit = async (url, lng,lat,days,key) => {
-  const res = await fetch(url +'&lat='+lat+'&lon='+lng+days+key);
+  let dayOfTrip = new Date(days);
+  let res;
+  if(calculateDaysToDate(dayOfTrip)<=7){
+  res = await fetch(url +weatherbit_API_type[1]+'&lat='+lat+'&lon='+lng+key);
+  }else{
+    let startDate = '&start_day='+months[dayOfTrip.getMonth()]+"-"+dayOfTrip.getDate();
+    let endDate = '&end_day='+months[dayOfTrip.getMonth()]+"-"+dayOfTrip.getDate();
+   res = await fetch(url +weatherbit_API_type[0]+'&lat='+lat+'&lon='+lng+startDate+endDate+key);
+  }
+
   try {
     const data = await res.json();
     console.log(data);
@@ -109,12 +117,60 @@ const postData = async (url = '', data = {}) => {
   }
 }
 
+function getNewDate(){
+let d = new Date();
+let newDate = months[d.getMonth()] + '/' + d.getDate()+ '/' + d.getFullYear();
+const date1 = new Date(newDate);
+return date1;
+}
+
+function calculateDaysToDate(date2){
+const date1 = getNewDate();
+console.log(date1);
+console.log(date2);
+var time_diff = date2.getTime() - date1.getTime(); 
+console.log("time_diff:")
+console.log(time_diff)
+const daysto= time_diff / (1000 * 3600 * 24); 
+
+console.log(daysto);
+
+return daysto;
+
+}
+
 //done 
-const generateContent = async () => {
+const generateContent = async (city,date) => {
   const request = await fetch('http://localhost:3000/load');
   try {
     const projectData = await request.json();
-    document.getElementById('results').innerHTML = projectData.country;
+
+    console.log(projectData.max_temp[0])
+
+    // data from project data 
+    const country = projectData.country;
+    const max_temp = projectData.max_temp;
+    const low_temp = projectData.low_temp;
+    const description = projectData.description;
+    const dateOfTrip = new Date(date);
+    const daysUntilTrip = calculateDaysToDate(dateOfTrip);
+
+    // refrences to UI
+    const title_container = document.getElementsByClassName('trip_title')[0];
+    const date_container = document.getElementsByClassName('trip_date')[0];
+    const daysToTrip_countainer = document.getElementsByClassName('trip_days_remaining')[0];
+    const weather_container = document.getElementsByClassName('trip_weather')[0];
+    const image = document.getElementById('img').setAttribute('src',projectData.img_url);
+    
+    // changing UI
+    title_container.innerHTML = 'Destination: \n \n'+country+", "+city +" on "+date;
+    date_container.innerHTML = date;
+    daysToTrip_countainer.innerHTML =' Days remaining until your trip \n \n'+daysUntilTrip;
+    // weather_container.innerHTML ='Max tempreature:\n \n'+max_temp+'\n \n Lowest Tempreture: \n \n'+low_temp+" \n \n"+description;
+
+    // trip_container.style.display = 'felx';
+
+  
   } catch (error) {
     console.log("error", error);
   }
